fda566c77775ad0e43a40fcebae418cc3f7f28b4
RatanakT <ratanakthan.tr@gmail.com>
2022-08-03 23:34:52 +0700

    change sidenavbar

--- resources/views/category/plumbing.blade.php
+++ resources/views/category/plumbing.blade.php
@@ -53,25 +53,7 @@
 
             </div>
         </div>
-        <div class="services p-4" style="width: 30%;">
-            <div class="shadow-lg px-4 py-5 bg-body rounded ">
-                <div class="d-flex justify-content-center">
-                    <h4>Other&nbsp;</h4>
-                    <h4 class="text-primary">Services</h4>
-                </div>
-                <hr style="border: none; border-bottom: 3px solid black">
-
-                <div class="px-3">
-                    <a href="{{ url('/electrical') }}" class=" text-black "><p>Electrical</p></a>
-                    <a href="{{ url('/plumbing') }}" class=" text-black"><p>Plumbing</p></a>
-                    <a href="{{ url('/cleaning') }}" class=" text-black"><p>Cleaning</p></a>
-                    <a href="{{ url('/painting') }}" class=" text-black"><p>Painting</p></a>
-                    <a href="{{ url('/flooring') }}" class=" text-black"><p>Flooring</p></a>
-                    <a href="{{ url('/carpentry') }}" class=" text-black"><p>Carpentry $ Carbinet</p></a>
-
-                </div>
-            </div>
-        </div>
+        @include('layout.sidenavbar')
     </div>
 </div>
 
